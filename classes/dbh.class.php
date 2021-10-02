697b482d7142e7835af1a7c699906f3e835cedc6
EU\terepdmi <dmitrijs.terepa@tietoevry.com>
2021-10-02 14:12:47 +0300

    test

--- classes/dbh.class.php
+++ classes/dbh.class.php
@@ -6,10 +6,11 @@ class Dbh
   public $user = "NsJrfw5ErR";
   public $pwd = "t5ySGQwak2";
   public $dbName = "NsJrfw5ErR";
+  public $charset = "utf8mb4";
 
   public function connect()
   {
-    $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbName;
+    $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbName  . ';charset=' . $this->charset;
     $pdo = new PDO($dsn, $this->user, $this->pwd);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     return $pdo;
