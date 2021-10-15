b12dfe1e2cc30529a09d18fa6302fe92b8b74fd6
EU\terepdmi <dmitrijs.terepa@tietoevry.com>
2021-10-15 19:05:40 +0300

    changes

--- index.php
+++ index.php
@@ -4,6 +4,12 @@ require_once("./templates/header.php");
 require_once("./objects/product.php");
 require_once("./objects/view_type.php");
 require_once("./objects/view_products.php");
+$param_prefix['Furniture'] = 'Dimensions';
+$param_prefix['Book'] = 'Weight';
+$param_prefix['DVD'] = 'Size';
+$param_postfix['Furniture'] = '';
+$param_postfix['Book'] = 'KG';
+$param_postfix['DVD'] = 'MB';
 ?>
 <nav class="navbar navbar-light">
   <div class="container-fluid">
@@ -25,10 +31,22 @@ require_once("./objects/view_products.php");
   $view->productsView();
   ?>
 
-</div>
+  <<<<<<< HEAD </div>
+    =======
+    if ($posts->getParams($post['id'])) {
+    if ($post['brand'] == "Furniture") {
+    foreach ($posts->getParams($post['id']) as $params) {
+    $param_html .= $params['param_value'] . "x";
+    }
+    $param_html = substr($param_html, 0, -1);
+    } else {
+    $params = $posts->getParams($post['id']);
+    $param_html .= $params[0]['param_value'];
+    }
+    >>>>>>> 8c3c32253f8a7f0bd50ff8c664f97aa7d99dd941
 
-<script src="./js/massDelete.js"></script>
+    <script src="./js/massDelete.js"></script>
 
-<?php
-require_once("./templates/footer.php");
-?>
+    <?php
+    require_once("./templates/footer.php");
+    ?>
